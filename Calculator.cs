394c981f973fc2adb5119082607523a2f7be65e6
hananwalker <hanan.walker@students.amikom.ac.id>
2021-03-12 16:49:04 +0700

    instalasi project

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,25 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-
+		int a = 10;
+		int b = 6;
+		
+		Console.Writeline)("Hasil Penambahan: {0} + {1}", a, b, Penambahan (a, b)));
+		Console.Writeline)("Hasil Pengurangan: {0} + {2}", a, b, Pengurangan (a, b)));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keliar");
+		Console.ReadKey();
 	}
+	
+	static int Penambahan(int a,  int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a,  int b)
+	{
+		return a - b;
+	}
+
 
 }
